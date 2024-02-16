5c7eb566fd2dc7b14bf6349f9c009fe6a3142094
nhota <thien.hieu2299@gmail.com>
2024-02-16 14:10:21 +0700

    more

---
+++ AppCS/AppCS/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace AppCS {
+    internal static class Program {
+        public static void Main(string[] args) {
+            ArrayAndHashing.Run();
+        }
+    }
+}
+
---
+++ csapp/Program.cs
@@ -0,0 +1,19 @@
+﻿class Solution
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = { 0, 1, 2, 2, 4 };
+        Console.WriteLine(ContainsDuplicate(nums));
+
+    }
+    public static bool ContainsDuplicate(int[] nums)
+    {
+        int[] storednums = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (storednums.Contains(nums[i])) return false;
+            else storednums[i] = nums[i];
+        }
+        return true;
+    }
+}
