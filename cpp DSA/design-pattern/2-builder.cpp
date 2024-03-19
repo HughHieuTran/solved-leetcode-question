b954b1da0e2dbbcbc6da9e86ff5583729ebcc2a9
Hieu <Admin@Kem>
2024-03-19 22:22:11 +0700

    hj

--- cpp DSA/design-pattern/2-builder.cpp
+++ cpp DSA/design-pattern/2-builder.cpp
@@ -3,101 +3,119 @@
 #include <vector>
 
 // Product: Pizza
-class Pizza {
+class Pizza
+{
 private:
     std::string dough;
     std::string sauce;
     std::vector<std::string> toppings;
 
 public:
-    void setDough(const std::string& dough) {
+    void setDough(const std::string &dough)
+    {
         this->dough = dough;
     }
 
-    void setSauce(const std::string& sauce) {
+    void setSauce(const std::string &sauce)
+    {
         this->sauce = sauce;
     }
 
-    void addTopping(const std::string& topping) {
-        toppings.push_back(topping);
+    void addTopping(const std::string &topping)
+    {
+        this->toppings.push_back(topping);
     }
 
-    void showPizza() const {
+    void showPizza() const
+    {
         std::cout << "Pizza with " << dough << " dough and " << sauce << " sauce." << std::endl;
         std::cout << "Toppings:" << std::endl;
-        for (const auto& topping : toppings) {
+        for (const auto &topping : toppings)
+        {
             std::cout << "- " << topping << std::endl;
         }
     }
 };
 
 // Builder Interface
-class PizzaBuilder {
+class PizzaBuilder
+{
 public:
     virtual void buildDough() = 0;
     virtual void buildSauce() = 0;
     virtual void buildToppings() = 0;
-    virtual Pizza* getPizza() = 0;
+    virtual Pizza *getPizza() = 0;
 };
 
 // Concrete Builder
-class HawaiianPizzaBuilder : public PizzaBuilder {
+class HawaiianPizzaBuilder : public PizzaBuilder
+{
 private:
-    Pizza* pizza;
+    Pizza *pizza;
 
 public:
-    HawaiianPizzaBuilder() {
+    HawaiianPizzaBuilder()
+    {
         pizza = new Pizza();
     }
 
-    void buildDough() override {
+    void buildDough() override
+    {
         pizza->setDough("thin crust");
     }
 
-    void buildSauce() override {
+    void buildSauce() override
+    {
         pizza->setSauce("tomato");
     }
 
-    void buildToppings() override {
+    void buildToppings() override
+    {
         pizza->addTopping("ham");
         pizza->addTopping("pineapple");
         pizza->addTopping("cheese");
     }
 
-    Pizza* getPizza() override {
+    Pizza *getPizza() override
+    {
         return pizza;
     }
 };
 
 // Director
-class PizzaDirector {
+class PizzaDirector
+{
 private:
-    PizzaBuilder* pizzaBuilder;
+    PizzaBuilder *pizzaBuilder;
 
 public:
-    void setPizzaBuilder(PizzaBuilder* builder) {
+    void setPizzaBuilder(PizzaBuilder *builder)
+    {
         pizzaBuilder = builder;
     }
 
-    Pizza* getPizza() {
+    Pizza *getPizza()
+    {
         return pizzaBuilder->getPizza();
     }
 
-    void constructPizza() {
+    void constructPizza()
+    {
         pizzaBuilder->buildDough();
         pizzaBuilder->buildSauce();
         pizzaBuilder->buildToppings();
     }
 };
 
-int main() {
+int main()
+{
     PizzaDirector director;
     HawaiianPizzaBuilder hawaiianPizzaBuilder;
 
     director.setPizzaBuilder(&hawaiianPizzaBuilder);
     director.constructPizza();
 
-    Pizza* pizza = director.getPizza();
+    Pizza *pizza = director.getPizza();
     pizza->showPizza();
 
     delete pizza; // Don't forget to delete dynamically allocated memory
